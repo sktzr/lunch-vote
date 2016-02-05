6deb4e39b2c65f17d3e8e1638671a0fe62494d6a
samanthakatzer <sam@radialogica.com>
2016-02-04 21:37:09 -0600

    fixed selection bug

--- app/components/check-box.js
+++ app/components/check-box.js
@@ -3,10 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   restaurant: null,
   selected: false,
+  count: 0,
+  canSelectMore: Ember.computed('count', function () {
+    return this.get('count') <= 2;
+  }),
   actions: {
     selected: function () {
-      this.toggleProperty('selected');
-      this.sendAction('action', this.get('restaurant'), this.get('selected'));
+      if (this.get('canSelectMore') || ((this.get('canSelectMore') === false) && (this.get('selected')))) {
+        this.toggleProperty('selected');
+        this.sendAction('action', this.get('restaurant'), this.get('selected'));
+      }
     }
   }
 });
